2551ff622de4a1407d8ab8e4c727c1b5ee9bb30a
Benjamin2024 <155924514+Benjamin2024@users.noreply.github.com>
2025-05-26 21:33:00 -0700

    Update medical_insurance_cost_prediction.py

--- medical_insurance_cost_prediction.py
+++ medical_insurance_cost_prediction.py
@@ -139,7 +139,7 @@ print(Y)
 
 """Splitting the data into Train and Test"""
 
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=2)
 
 print(X.shape, X_train.shape, X_test.shape)
 
